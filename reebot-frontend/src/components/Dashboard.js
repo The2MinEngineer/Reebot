65a49aeda2acc777a675158b9c2ef7fd21f1695f
the2MinEngineer <ifeanyiemmanueljoseph@gmail.com>
2024-01-09 04:00:18 +0100

    dashboard

--- reebot-frontend/src/components/Dashboard.js
+++ reebot-frontend/src/components/Dashboard.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { ReactComponent as Reebot } from "../assets/Icon.svg";
+import { ReactComponent as Naira } from "../assets/Naira.svg";
 
 const Dashboard = () => {
 	return (
 		<div className="flex text-primary-dark space-x-5">
-			<div className="bg-white rounded-[20px] p-5 w-1/3">
+			<div className="bg-white rounded-[20px] p-5 w-1/4">
 				<div className="mt-5">
 					<Reebot />
 					<h1 className="text-primary-dark text-2xl font-bold mt-[10px]">
@@ -21,13 +22,34 @@ const Dashboard = () => {
 				</p>
 			</div>
 
-			<div className="w-2/3">
-                <div className="bg-white rounded-[20px] p-5">
-                    <p>hello</p>
-                </div>
+			<div className="w-2/4">
+				<div className="bg-white rounded-[20px] p-10">
+					<div className="flex justify-between items-center mb-10">
+						<p className="text-[18px] font-bold text-[#181818]">EXPENSES</p>
+						<div className="flex items-center">
+							<p className="text-[18px] font-semibold text-[#181818] text-opacity-70">
+								Activate All:
+							</p>
+						</div>
+					</div>
+					<div className="flex items-center space-x-[10px]">
+						<Naira />
+						<p className="text-[64px] font-extrabold text-[#181818]">
+							0<span className="text-[48px]">.</span>
+							<span className="text-[48px]">00</span>
+						</p>
+					</div>
+					<div className="mt-[40px]">
+						<p className="text-[18px] font-semibold text-[#181818]">
+							Active Subscriptions: 0 of 0
+						</p>
+					</div>
+				</div>
+
+				<div>Hello</div>
 			</div>
 
-			<div className="bg-white rounded-[20px] p-5 w-1/3">
+			<div className="bg-white rounded-[20px] p-5 w-1/4">
 				<h2 className="text-lg font-bold mt-8">Welcome, Isaac</h2>
 				<p className="mt-5">
 					You can now proceed to add subscriptions to your account dashboard by
